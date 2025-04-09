1017121c22b8fed2909cf05f49990a2a3253f175
Augustin Chan <aug@iterative.day>
2025-04-10 00:06:47 +0800

    Refactor package.json and tsup.config.ts for improved module exports and build configuration; switch to using defineConfig for tsup settings.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,23 +1,20 @@
-import type { Options } from 'tsup'
+import { defineConfig } from 'tsup'
 
-const env = process.env.NODE_ENV
-
-export const tsup: Options = {
-  splitting: true,
-  sourcemap: false, // source map is only available in production
-  clean: true, // rimraf dist
-  dts: true, // generate dts file for main module
-  format: ['cjs', 'esm'], // generate cjs and esm files
-  minify: env === 'production',
-  bundle: env === 'production',
-  skipNodeModulesBundle: true,
-  entryPoints: ['src/index.ts'],
-  watch: env === 'development',
-  target: 'es2020',
-  outDir: env === 'production' ? 'dist' : 'lib',
-  entry: ['src/**/*.ts'],
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  clean: true,
+  external: ['react'],
+  sourcemap: true,
   treeshake: true,
+  splitting: true,
+  minify: process.env.NODE_ENV === 'production',
+  outDir: 'dist',
+  target: 'es2020',
   esbuildOptions: (options) => {
-    options.drop = env === 'production' ? ['console', 'debugger'] : undefined
+    if (process.env.NODE_ENV === 'production') {
+      options.drop = ['console', 'debugger']
+    }
   },
-}
+})
